a4b6c68c8ae203f714c1821cc08ab3662f5990a0
Chris Huber <chubes@extrachill.com>
2026-08-25 17:59:22 +0000

    fix: normalize persisted integer post fields

--- inc/migration.php
+++ inc/migration.php
@@ -113,7 +113,8 @@ function ec_link_page_migration_post_fields( $post ) {
 	$fields = array( 'post_author', 'post_date', 'post_date_gmt', 'post_content', 'post_title', 'post_excerpt', 'post_status', 'comment_status', 'ping_status', 'post_password', 'post_name', 'to_ping', 'pinged', 'post_modified', 'post_modified_gmt', 'post_content_filtered', 'post_parent', 'guid', 'menu_order', 'post_type', 'post_mime_type' );
 	$data   = array( 'ID' => (int) $post->ID );
 	foreach ( $fields as $field ) {
-		$data[ $field ] = isset( $post->{$field} ) ? $post->{$field} : '';
+		$value          = isset( $post->{$field} ) ? $post->{$field} : '';
+		$data[ $field ] = in_array( $field, array( 'post_parent', 'menu_order' ), true ) ? (int) $value : $value;
 	}
 	return $data;
 }
